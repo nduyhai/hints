cfdef2ea0f8db21d23db8a11aa1b199b70ddd288
Hai Nguyen <haind.vng@gmail.com>
2021-06-20 14:11:48 +0700

    add hint with resource

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # hints
 Spring native example
+
+## Build
+
+```shell
+mvn -Pnative -DskipTests package
+```
+
+### Run
+
+```shell
+./target/hints
+```
+
+## Test
+
+```shell
+mvn -Pnative test
+```
--- src/main/java/com/example/hints/HintsApplication.java
+++ src/main/java/com/example/hints/HintsApplication.java
@@ -4,20 +4,47 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Slf4j
+@NativeHint(options = {"--enable-https"})
 @SpringBootApplication
 public class HintsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HintsApplication.class, args);
     }
+
+    @Bean
+    public ApplicationRunner runner(@Value("classpath:/greeting.txt") Resource resource) {
+        return args -> {
+            try (InputStream in = resource.getInputStream()) {
+                List<String> lines = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)).lines().collect(Collectors.toList());
+                log.info("There are {} lines", lines.size());
+
+            }
+        };
+    }
+
 
 }
 
